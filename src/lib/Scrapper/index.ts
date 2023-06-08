64f88fbb11619b87945ce1e98f9a4872e94607fc
Waqif Alam <waqifalam@gmail.com>
2023-06-08 13:48:27 +0600

    add TheNewsDaily integration

--- src/lib/Scrapper/index.ts
+++ src/lib/Scrapper/index.ts
@@ -1,9 +1,12 @@
 import SBSScrapper from "./SBS"
+import TheNewsDailyScrapper from "./TheNewsDaily";
 
 const getScrapper = (scrapperId: string) => {
   switch (scrapperId) {
     case 'SBS':
       return SBSScrapper
+    case 'TheNewsDaily':
+      return TheNewsDailyScrapper;
     default:
       throw new Error('Scrapper ID does not exist');
   }
